d5ad951d4ead0ad3eed17a5ac956d43710bad605
rodolfo <45057543+Srodo95@users.noreply.github.com>
2020-02-12 11:47:09 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,9 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+    var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
+    alert (laHora);
+    switch(laHora>=7 && laHora<=11){
+        case true:
+            alert("es de mañana!");
+            break;
+        case false:
+            alert("No es de mañana");
+            break;
+    }
 	
 	
 
