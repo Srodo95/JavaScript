e87630ba090817b8bea8f63318f260669030bdcc
rodolfo <45057543+Srodo95@users.noreply.github.com>
2020-02-13 10:08:50 -0300

    Update cuatro.js

--- 2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+    var primerNumero = prompt("Por favor ingrese el primer numero");
+    var segundoNumero = prompt("Por favor ingrese el segundo numero");
+    var resultado;
 
+    if(primerNumero == segundoNumero){
+        alert("son iguales, los concateno ----> " + primerNumero + segundoNumero);
+    }
+    if(primerNumero > segundoNumero){
+        resultado = parseInt(primerNumero) - parseInt(segundoNumero);
+    }else{
+        resultado = parseInt(primerNumero) + parseInt(segundoNumero);
+    }
+    if(resultado>10){
+        alert("la suma es "+resultado+" y es mayor a 10");
+    }
 }
