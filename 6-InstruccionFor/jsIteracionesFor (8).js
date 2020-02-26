89e8b1d3a0abacda287b9afa4009c2fb317785b7
rodolfo <45057543+Srodo95@users.noreply.github.com>
2020-02-26 12:10:02 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -7,12 +7,26 @@ function mostrar()
             validarPar++;
         }
     }
-    if(validarPar>1){
+    if(validarPar>1 || numero <= 1){
         alert("El numero no es primo");
     }else{
         alert("El numero es primo");
     }
 
+
+    /*
+    var flag=true;
+        for(var i=2;i<num-1;i++){
+            if(num%i==0){
+                flag=false
+            }
+        }
+        if(flag==true){
+            alert("El numero" + num  +" es primo ");
+        }else{
+            alert("El numero " + num + "no es primo")
+        }
+    */ 
 
 
 }//FIN DE LA FUNCIÓN
